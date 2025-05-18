8537078dd117ff75363f6ec7cfaf223f775743eb
Ronell Warmuth <slhsashd@proton.me>
2025-05-18 18:38:06 -0400

    connect frontend to backend with VITE_API_URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
+  define: {
+    'process.env': {}, // Patch for packages using process.env
+  },
 })
